984bd14ef00157ce6cda7b199bf19f4012de7f4c
AleWasser <alejowasser@gmail.com>
2018-10-11 10:44:52 -0300

    Aviso de rediseño

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 AOS.init({
     startEvent: 'DOMContentLoaded'
 });
+
+let close = document.querySelector('.aviso-icon');
+
+close.addEventListener('click', (e) => {
+    document.querySelector('.aviso').classList.add('hidden');
+});
